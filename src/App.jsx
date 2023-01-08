615cd86440599955359528733a52cc926cb0c212
shufol islam <shufolislam1255@gmail.com>
2023-01-08 19:39:12 +0600

    material ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-
+    <div className="App">
+      <Navbar></Navbar>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <button className="btn btn-accent">Button</button>
+        </div>
+    );
+};
+
+export default Navbar;
