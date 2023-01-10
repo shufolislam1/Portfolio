7da86ade50eedb8ef7ec15365f30738aed850cf4
shufol islam <shufolislam1255@gmail.com>
2023-01-10 13:24:52 +0600

    owl carousel successfully implemented

--- globals.d.ts
+++ globals.d.ts
@@ -1,2 +1,3 @@
 declare module 'daisyui';
-declare module 'mui-scroll-trigger'
+declare module 'mui-scroll-trigger';
+declare module 'react-owl-carousel2';
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.min.css';
-import 'owl.carousel/dist/assets/owl.theme.default.min.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 
 const Projects = () => {
     return (
         <div className='mt-48'>
             <h1 className='text-red-600 font-serif mb-4'>Projects</h1>
-            <OwlCarousel  className='owl-theme' loop margin={10} nav={true} autoPlay={true}>
+            <OwlCarousel  className='owl-theme' nav margin={10} autoplay autoplayTimeout={1000}  loop>
                 <div className='item'>
                     <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
                         <div className="hero-overlay bg-opacity-60"></div>
