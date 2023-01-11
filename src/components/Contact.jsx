1ea18e594177b5afb77773adb6059db6ea387e95
shufol islam <shufolislam1255@gmail.com>
2023-01-11 16:32:08 +0600

    sweetalert done and contact 100% done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2'
 
 const Contact = () => {
     const form = useRef();
 
     const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_hvvu5e6', 'template_3h7sj6k', form.current, 'Rv_5cHasR0f7nx5ig')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+        e.preventDefault();
+
+        emailjs.sendForm('service_hvvu5e6', 'template_3h7sj6k', form.current, 'Rv_5cHasR0f7nx5ig')
+            .then((result) => {
+                if (result) {
+                    Swal.fire({
+                        title: 'success!',
+                        text: 'You are done.',
+                        icon: 'success',
+                        confirmButtonText: 'Ok'
+                    })
+                }
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
     };
     return (
         <div id='contact'>
@@ -20,9 +30,9 @@ const Contact = () => {
             <div className='grid grid-cols-2 my-36'>
                 <div className='w-3/5'>
                     <form ref={form} onSubmit={sendEmail}>
-                        <input type="text" placeholder="Name" name='from_name' className="input input-bordered w-full max-w-xl" /> <br />
-                        <input type="text" placeholder="Email" name='from_email' className="input input-bordered w-full max-w-xl my-4" /> <br />
-                        <input type="text" placeholder="Subject" className="input input-bordered w-full max-w-xl" /> <br />
+                        <input type="text" placeholder="Name" name='name' className="input input-bordered w-full max-w-xl" /> <br />
+                        <input type="text" placeholder="Email" name='email' className="input input-bordered w-full max-w-xl my-4" /> <br />
+                        <input type="text" placeholder="Subject" name='subject' className="input input-bordered w-full max-w-xl" /> <br />
                         <textarea className="textarea w-full max-w-xl my-4" name='message' placeholder="Write your message"></textarea> <br />
                         <input type="submit" value="Send Message" className="btn btn-wide" />
                     </form>
