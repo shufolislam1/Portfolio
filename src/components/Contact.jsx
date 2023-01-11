e99f196477e59834fd8734c7c7f344d09e07cdf2
shufol islam <shufolislam1255@gmail.com>
2023-01-11 16:11:34 +0600

    emailjs implemented to send email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_hvvu5e6', 'template_3h7sj6k', form.current, 'Rv_5cHasR0f7nx5ig')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     return (
         <div id='contact'>
             <h1 className='text-red-600 font-serif my-10'>Contact Me</h1>
             <div className='grid grid-cols-2 my-36'>
                 <div className='w-3/5'>
-                    <form action="" method="post">
-                        <input type="text" placeholder="Name" className="input input-bordered w-full max-w-xl" /> <br />
-                        <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xl my-4" /> <br />
+                    <form ref={form} onSubmit={sendEmail}>
+                        <input type="text" placeholder="Name" name='from_name' className="input input-bordered w-full max-w-xl" /> <br />
+                        <input type="text" placeholder="Email" name='from_email' className="input input-bordered w-full max-w-xl my-4" /> <br />
                         <input type="text" placeholder="Subject" className="input input-bordered w-full max-w-xl" /> <br />
-                        <textarea className="textarea w-full max-w-xl my-4" placeholder="Write your message"></textarea> <br />
+                        <textarea className="textarea w-full max-w-xl my-4" name='message' placeholder="Write your message"></textarea> <br />
                         <input type="submit" value="Send Message" className="btn btn-wide" />
                     </form>
                 </div>
-                {/* <div>
-                    <LocationOnIcon></LocationOnIcon><br /> 
-                    <p className='font-serif my-2 font-bold text-xl'>Dhanmondi-8, Dhaka, Bangladesh.</p>
-                    <EmailIcon></EmailIcon> <br />
-                    <p className='font-serif my-2 font-bold text-xl'>smshufolislam@gmail.com</p>
-                    <GitHubIcon></GitHubIcon> <br />
-                    <p className='font-serif my-2 font-bold text-xl'><a href="https://github.com/shufolislam1">Github</a></p>
-                    <LinkedInIcon></LinkedInIcon>
-                    <p className='font-serif my-2 font-bold text-xl'><a href="https://www.linkedin.com/in/shufol-islam-441b0922b/">LinkedIn</a></p>
-                </div> */}
-                <img src='https://i.ibb.co/zSvt03M/contact.gif' />
+                <div>
+                    <img src='https://i.ibb.co/zSvt03M/contact.gif' />
+                </div>
             </div>
         </div>
     );
