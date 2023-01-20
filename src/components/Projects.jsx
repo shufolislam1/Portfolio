6bbecddd9fe20c655bfdc24f64ad7721a22f7a2a
shufol islam <shufolislam1255@gmail.com>
2023-01-20 14:31:49 +0600

    project and services responsive updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,14 +10,15 @@ const Projects = () => {
     return (
         <div className='mt-48' id='projects'>
             <h1 className='text-red-600 font-serif mb-20 text-5xl'>Projects</h1>
+            <div className=''>
             <OwlCarousel className='owl-theme grid' nav margin={10} autoplay autoplayTimeout={2000} loop>
-                <div className='item sm:grid-cols-1'>
+                <div className='item'>
                     <Card isHoverable isPressable>
                         <div className="hero min-h-screen" style={{ backgroundImage: `url("https://i.ibb.co/TYW8QJQ/sawari-spark-shufol-netlify-app.png")` }}>
                             <div className="hero-overlay bg-opacity-60"></div>
                             <div className="hero-content text-center text-neutral-content">
                                 <div className="max-w-md">
-                                    <h1 className="mb-5 text-5xl font-bold">Sawari Spark</h1>
+                                    <h1 className="mb-5 lg:text-5xl sm:text-2xl font-bold">Sawari Spark</h1>
                                     <a target='_blank' href="https://sawari-spark-shufol.netlify.app/"><button className="btn"> View Live</button></a> 
                                 </div>
                             </div>
@@ -30,7 +31,7 @@ const Projects = () => {
                             <div className="hero-overlay bg-opacity-60"></div>
                             <div className="hero-content text-center text-neutral-content">
                                 <div className="max-w-md">
-                                    <h1 className="mb-5 text-5xl font-bold">Party Center</h1>
+                                    <h1 className="mb-5 lg:text-5xl sm:text-2xl font-bold">Party Center</h1>
                                     <a target='_blank' href="https://awesome-galileo-7aef9e.netlify.app/"><button className="btn">View Live</button> </a> 
                                 </div>
                             </div>
@@ -38,6 +39,7 @@ const Projects = () => {
                     </Card>
                 </div>
             </OwlCarousel>
+            </div>
         </div>
     );
 };
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,12 +5,12 @@ const Services = () => {
     return (
         <div  className='mt-32' id='services'>
             <h1 className='text-red-600 font-serif mb-4 text-5xl'>Services</h1>
-            <div className='grid grid-cols-3 gap-6 my-20'>
+            <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-6 my-20'>
                 <Card isHoverable isPressable>
                 <div className="card lg:card-side sm:grid-cols-1 shadow-xl ">
                     <figure><img src="https://i.ibb.co/Lpq7RdV/frontend.gif" alt="Album" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title font-serif">Frontend Development</h2>
+                        <h2 className="card-title font-serif ">Frontend Development</h2>
                         <p className=' font-serif'>Click the button to listen on Spotiwhy app.</p>
                     </div>
                 </div>
@@ -19,7 +19,7 @@ const Services = () => {
                 <div className="card lg:card-side sm:grid-cols-1 shadow-xl ">
                     <figure><img src="https://i.ibb.co/JCrJw2d/dazzle-man-programmer-writing-code-on-a-laptop-1.gif" alt="Album" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title font-serif">Fullstack Development</h2>
+                        <h2 className="card-title font-serif ">Fullstack Development</h2>
                         <p className=' font-serif'>Click the button to listen on Spotiwhy app.</p>
                     </div>
                 </div>
