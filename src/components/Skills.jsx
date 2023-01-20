e62564aa2c2f8afc85cdd27443dafd458d76920f
shufol islam <shufolislam1255@gmail.com>
2023-01-20 23:11:29 +0600

    express logo added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,19 +2,9 @@ import React from 'react';
 
 const Skills = () => {
     return (
-        <div  className='mt-32' id='skill'>
+        <div className='mt-32' id='skill'>
             <h1 className='text-red-600 font-serif mb-8 text-5xl'>My Skills</h1>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4'>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://i.ibb.co/M2sbmgq/react.gif" alt="react" className="rounded-xl" />
-                    </figure>
-                </div>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://i.ibb.co/5jM2pnf/nodejs.gif" alt="nodejs" className="rounded-xl" />
-                    </figure>
-                </div>
                 <div className="card w-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
                         <img src="https://i.ibb.co/DbFFDT6/javascript.gif" alt="javascript" className="rounded-xl" />
@@ -22,7 +12,7 @@ const Skills = () => {
                 </div>
                 <div className="card w-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
-                        <img src="https://i.ibb.co/xXQNjjx/mongodb.gif" alt="mongodb" className="rounded-xl" />
+                        <img src="https://i.ibb.co/M2sbmgq/react.gif" alt="react" className="rounded-xl" />
                     </figure>
                 </div>
                 <div className="card w-96 bg-base-100 shadow-xl">
@@ -42,7 +32,22 @@ const Skills = () => {
                 </div>
                 <div className="card w-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
-                        <img src="https://img.icons8.com/color/240/null/tailwindcss.png" alt="css3" className="rounded-xl" />
+                        <img src="https://img.icons8.com/color/240/null/tailwindcss.png" alt="tailwindCSS" className="rounded-xl" />
+                    </figure>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/5jM2pnf/nodejs.gif" alt="nodejs" className="rounded-xl" />
+                    </figure>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/xXQNjjx/mongodb.gif" alt="mongodb" className="rounded-xl" />
+                    </figure>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-24 ">
+                        <img src="https://i.ibb.co/T4LDxdW/Expressjs.png" alt="mongodb" className="rounded-xl" />
                     </figure>
                 </div>
             </div>
