33790b8f0a01a95823e5a3b18f81f80b9a7c4f1a
shufol islam <shufolislam1255@gmail.com>
2023-01-21 23:21:01 +0600

    about me actual text added and all done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                 </div>
                 <div className='text-left ml-16'>
                 <h6 className='font-serif text-red-600 text-4xl'>Shufol Islam</h6>
-                <p className='font-serif mt-4 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo commodi harum quod alias aperiam doloribus nobis ratione animi laudantium fugit quo vero, minus deserunt tempore repellendus nihil nesciunt nam accusantium.</p>
+                <p className='font-serif mt-4 text-justify text-lg'>I am currently studying in the CSE department at Sonargaon University. I am comfortable developing projects with Html 5, CSS 3, Tailwind CSS, Daisy UI, React, and other React libraries. Also have knowledge about version controling system github. Besides,  Passionate about exploring new technologies, learning new things by doing a project, and being dedicated to solving problems. I am also curious about Competetive Programming and trying to be a good problem solver. I am always open to new projects and would love to explore new things together. If you have any queries or have opportunities, don't hesitate to knock.</p>
                 <a href="#contact"><button className="btn font-serif mt-8"> Let ' s Connect !</button></a>
                 </div>
             </div>
