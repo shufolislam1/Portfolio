4a0969eb0ec6a47be391926a60ac791157c7bfb2
shufol islam <shufolislam1255@gmail.com>
2023-08-03 13:13:55 +0600

    last push

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-    container: {
-      center: true,
-    },
+  // theme: {
+    
+  //   extend: {},
+  //   container: {
+  //     center: true,
+  //   },
+  // },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      
+    ],
   },
   plugins: [require("daisyui")],
 }
